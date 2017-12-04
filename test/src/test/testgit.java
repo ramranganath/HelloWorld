6df7cbc5d22fa16181e440f51f11fb97134de274
Ramesh <Ramesh@Ananya>
2017-12-04 10:27:42 +0530

    new Project committed

---
+++ test/src/test/testgit.java
@@ -0,0 +1,10 @@
+package test;
+
+public class testgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("testing push proj from Eclipse");
+	}
+
+}
